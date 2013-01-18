b4c1810c250f34b222870be170ce9520deccfb5c
Manodasan W <manodasan@users.sourceforge.net>
2013-01-18 10:18:52 -0500

    Added configuration parameters

--- Code/memory.h
+++ Code/memory.h
@@ -3,6 +3,10 @@
 
 #define __SVC_0  __svc_indirect(0)
 
+// Configuration parameters for memory
+#define NUM_MEMORY_BLOCKS 20
+#define MEMORY_BLOCK_SIZE 128
+
 void* k_request_memory_block(void);
 #define request_memory_block() _request_memory_block((unsigned int)k_request_memory_block)
 extern void* _request_memory_block(unsigned int p_func) __SVC_0;
