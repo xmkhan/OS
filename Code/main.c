73fd11c83af890ad28ea6778f76ef7e752b1b1e0
unknown <ms22khan@ece-rtos19.NEXUS.UWATERLOO.CA>
2013-01-18 12:09:49 -0500

    Added simulator printf

--- Code/main.c
+++ Code/main.c
@@ -1,16 +1,29 @@
+#define _DEBUG_
+
 #include <LPC17xx.h>
 #include "uart_polling.h"
-//#include <stdio.h>
+#ifdef _DEBUG_
+#include <stdio.h>
+#endif
 #include "memory.h"
 
 MemNodeHead m;
 volatile unsigned int ret_val;
 
+extern unsigned int Image$$RW_IRAM1$$ZI$$Limit;
+
+
 int main() {
 	SystemInit();
-  
+  __disable_irq();
+	uart0_init();
+	__enable_irq();
+	
   __set_CONTROL(__get_CONTROL() | BIT(0));  
 
   ret_val = (unsigned int) request_memory_block();
+#ifdef _DEBUG_
+	printf("%x\n\r", &Image$$RW_IRAM1$$ZI$$Limit);
+#endif
 	return 0;
 }
