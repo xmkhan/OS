361e8d6817db2d9e82eb9c99f827f93b628fff41
Mohammad Khan <xmoh.khan@gmail.com>
2013-01-21 10:13:46 -0500

    Add boilerplate to process.h for setting/getting priorities

--- Code/process.h
+++ Code/process.h
@@ -5,12 +5,25 @@
 #define __SVC_0  __svc_indirect(0)
 #endif
 
+
 typedef struct Process {
-	int type;
+	uint8_t priority;
+  
 } Process;
+
+
+Process NullProcess;
 
 extern int k_release_processor(void);
 #define release_processor() _release_processor((unsigned int)k_release_processor)
 extern int _release_processor(unsigned int p_func) __SVC_0;
+
+extern int k_set_process_priority(void);
+#define set_process_priority() _set_process_priority((unsigned int)k_set_process_priority)
+extern int _set_process_priority(unsigned int p_func) __SVC_0;
+
+extern int k_get_process_priority(void);
+#define get_process_priority() _get_process_priority((unsigned int)k_get_process_priority)
+extern int _get_process_priority(unsigned int p_func) __SVC_0;
 
 #endif
