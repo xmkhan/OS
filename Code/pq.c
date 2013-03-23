eac0e2b266bd0b392c9a8c2b284fec88c4a0b954
Manodasan W <manodasan@users.sourceforge.net>
2013-03-23 17:19:18 -0400

    Fix to memory blocked process not being unblocked.

--- Code/pq.c
+++ Code/pq.c
@@ -69,12 +69,11 @@ PCB* get_process(PCB *pq[]) {
   volatile PCB *pr_head = NULL;
   for(; i < NUM_PRIORITIES; ++i)
   {
-     pr_head = pq[i];
-     while (pr_head != NULL)
-     {
-       pr_head = pr_head->next;
-     }
-     if (pr_head != NULL) return (PCB*) pr_head;
+    pr_head = pq[i];
+    if(pr_head !=  NULL)
+    {
+      return (PCB*) pr_head;
+    }
   }
   return 0; // return NULL
 }
